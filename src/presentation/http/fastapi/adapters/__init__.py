05eaa26eb48909349563eec9281d4e592ee784f9
Alberto S. Mercado <109180196+A1b3rt0M3rcad0@users.noreply.github.com>
2026-07-31 09:57:41 -0300

    feat: add FastAPI HTTP interface

---
+++ src/presentation/http/fastapi/adapters/__init__.py
@@ -0,0 +1,4 @@
+from src.presentation.http.fastapi.adapters.request_adapter import adapt_request
+from src.presentation.http.fastapi.adapters.response_adapter import adapt_response
+
+__all__ = ["adapt_request", "adapt_response"]
