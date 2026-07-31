71fe19899c35fd9e20b35db143a4e7a41d9266c2
Alberto S. Mercado <109180196+A1b3rt0M3rcad0@users.noreply.github.com>
2026-07-31 12:20:27 -0300

    feat(auth): export authentication router

--- src/presentation/http/fastapi/routes/__init__.py
+++ src/presentation/http/fastapi/routes/__init__.py
@@ -1,13 +1,13 @@
+from src.presentation.http.fastapi.routes.auth import router as auth_router
 from src.presentation.http.fastapi.routes.customers import router as customers_router
-from src.presentation.http.fastapi.routes.satisfaction_ratings import (
-    router as satisfaction_ratings_router,
-)
+from src.presentation.http.fastapi.routes.satisfaction_ratings import router as satisfaction_ratings_router
 from src.presentation.http.fastapi.routes.tags import router as tags_router
 from src.presentation.http.fastapi.routes.ticket_tags import router as ticket_tags_router
 from src.presentation.http.fastapi.routes.tickets import router as tickets_router
 from src.presentation.http.fastapi.routes.users import router as users_router
 
 __all__ = [
+    "auth_router",
     "customers_router",
     "satisfaction_ratings_router",
     "tags_router",
