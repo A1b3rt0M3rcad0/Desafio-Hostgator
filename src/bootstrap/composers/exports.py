5261f07a6b7f180c8be0ecaf19e5740ad0e0f073
Alberto S. Mercado <109180196+A1b3rt0M3rcad0@users.noreply.github.com>
2026-07-31 22:54:34 -0300

    feat: compose data and metrics exports

---
+++ src/bootstrap/composers/exports.py
@@ -0,0 +1,48 @@
+from src.application.use_cases.exports import (
+    ExportData,
+    ExportMetrics,
+    GetExportCatalog,
+    PreviewDataExport,
+)
+from src.bootstrap.composers.database import DATABASE_ENGINE
+from src.infra.database.analytics import SqlAlchemyAnalyticsQueryRepository
+from src.infra.database.exports import SqlAlchemyDataExportRepository
+from src.infra.database.transactional_handler import TransactionalHandler
+from src.infra.database.unit_of_work import UnitOfWork
+from src.infra.reports.writers import DefaultReportWriterFactory
+from src.presentation.http.controllers.exports import (
+    ExportDataController,
+    ExportMetricsController,
+    GetExportCatalogController,
+    PreviewDataExportController,
+)
+
+
+def get_export_catalog_composer() -> TransactionalHandler:
+    unit_of_work = UnitOfWork(DATABASE_ENGINE)
+    repository = SqlAlchemyDataExportRepository(unit_of_work)
+    controller = GetExportCatalogController(GetExportCatalog(repository))
+    return TransactionalHandler(unit_of_work, controller.handle)
+
+
+def preview_data_export_composer() -> TransactionalHandler:
+    unit_of_work = UnitOfWork(DATABASE_ENGINE)
+    repository = SqlAlchemyDataExportRepository(unit_of_work)
+    controller = PreviewDataExportController(PreviewDataExport(repository))
+    return TransactionalHandler(unit_of_work, controller.handle)
+
+
+def export_data_composer() -> TransactionalHandler:
+    unit_of_work = UnitOfWork(DATABASE_ENGINE)
+    repository = SqlAlchemyDataExportRepository(unit_of_work)
+    writer_factory = DefaultReportWriterFactory()
+    controller = ExportDataController(ExportData(repository, writer_factory))
+    return TransactionalHandler(unit_of_work, controller.handle)
+
+
+def export_metrics_composer() -> TransactionalHandler:
+    unit_of_work = UnitOfWork(DATABASE_ENGINE)
+    repository = SqlAlchemyAnalyticsQueryRepository(unit_of_work)
+    writer_factory = DefaultReportWriterFactory()
+    controller = ExportMetricsController(ExportMetrics(repository, writer_factory))
+    return TransactionalHandler(unit_of_work, controller.handle)
